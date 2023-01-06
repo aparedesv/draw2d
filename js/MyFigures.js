6951b28ec165977524bfef16baa19b06e6f47f8d
andreu paredes <aparedesverdaguer@gmail.com>
2023-01-06 12:34:42 +0100

    contextMenu (botó dret)

--- js/MyFigures.js
+++ js/MyFigures.js
@@ -1,14 +1,56 @@
 
 var MySquare = draw2d.shape.basic.Rectangle.extend({
 
-    /**
-     * sense el init funciona igual???
-     * suposo que a mida que anem requerint funcionalitats caldrà...
-     */
-    /* init : function(attr, setter, getter)
+    onContextMenu: function(x, y)
     {
-        this._super($.extend({radius:0},attr), setter, getter); 
-    }, */
+
+        var shape = this.shape[0];
+        $(shape).attr('id', this.getId());
+        var id = "#" + $(shape).attr('id');
+        $.contextMenu({
+            selector: id,
+            autoHide: true,
+            events:
+                                {
+                                    hide: function () {
+                                        $.contextMenu('destroy');
+                                    }
+                                },
+            callback: $.proxy(function (key, options) {
+                switch (key) {
+                    case "red":
+                        this.setColor("ff0000");
+                        break;
+                    case "green":
+                        this.setColor("00ff00");
+                        break;
+                    case "blue":
+                        this.setColor("0000ff");
+                        break;
+                    case "delete":
+                       //  without undo/redo support
+                       //  this.getCanvas().removeFigure(this);
+    
+                       //  with undo/redo support
+                        var cmd = new draw2d.command.CommandDelete(this);
+                        this.getCanvas().getCommandStack().execute(cmd);
+    
+                    default:
+                        break;
+                }
+    
+            }, this),
+            x: x,
+            y: y,
+            items:
+                    {
+                        "red": { name: "Red" }, // callback: function () { return true; } },
+                        "green": { name: "Green" },
+                        "blue": { name: "Blue" },
+                        "delete": { name: "Delete" }
+                    }
+        });
+    },
 
     onDoubleClick: function()
     {
