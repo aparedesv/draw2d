1310a4cda47769194458dd7f0290c04eda941e84
andreu paredes <aparedesverdaguer@gmail.com>
2023-04-13 16:23:31 +0200

    MyFigures

--- js/MyFigures-old.js
+++ js/MyFigures-old.js
@@ -152,9 +152,6 @@ function showOptions(id, action) {
             case "segment":
                 optionsSegment(modalContent, id);
                 break;
-            case "email":
-                optionsEmail(modalContent, id);
-                break;
         
             default:
                 break;
@@ -175,7 +172,7 @@ function showOptions(id, action) {
 
 /**
  * Create and display options inside the modal according 
- * to the type `segment`
+ * to their type (segment, form, mail, etc.)
  * 
  * @param {object} modalContent HTML element
  * @param {string} idModal Modal id
@@ -242,64 +239,80 @@ function optionsSegment(modalContent, idModal) {
 
 }
 
-/**
- * Create and display options inside the modal according 
- * to the type `email`
- * 
- * @param {object} modalContent HTML element
- * @param {string} idModal Modal id
- */
-function optionsEmail(modalContent, idModal) {
-    const wrapperDiv = document.createElement('div');
-    wrapperDiv.classList.add('content-wrapper');
+let selectedOption = "";
 
-    for (let i = 1; i <= 4; i++) {
-        const div = document.createElement('div');
-        div.classList.add(`div${i}`);
-
-        if (i === 1) {
-            div.textContent = 'Select Email:';
-        }        
+var MyCustomFigureIcon = draw2d.shape.basic.Rectangle.extend({
+    
+    // Override the createShapeElement() method to create a custom HTML element
+    createShapeElement: function() {
+        var shape = this._super();
         
-        if (i === 2) {
-            const select = document.createElement('select');
-            select.classList.add('border-2');
-            const option1 = document.createElement('option');
-            const option2 = document.createElement('option');
-            const option3 = document.createElement('option');
-            option1.textContent = 'Select email...';
-            option2.textContent = 'Email A';
-            option3.textContent = 'Email List N';
-            select.addEventListener("change", (event) => {
-                const figure = app.canvas.getFigure(idModal);
-                if (figure) {
-                    figure.setText("Send Email: " + event.target.value);
-                }
-            });
-            select.appendChild(option1);
-            select.appendChild(option2);
-            select.appendChild(option3);
-            div.appendChild(select);
-        }
-
-        if (i === 3) {
-            const button = document.createElement('button');
-            button.textContent = 'New Email';
-            button.classList.add('btn-action');
-            div.appendChild(button);
-        }
-
-        if (i === 4) {
-            const button = document.createElement('button');
-            button.textContent = 'Save';
-            button.classList.add('btn-save');
-            div.appendChild(button);
-        }
+        this.icon = new draw2d.shape.basic.Image({
+            path: "./assets/icons/" + this.userData[0],
+            width: 35,
+            height: 35,
+        });
         
-        wrapperDiv.appendChild(div);
+        this.add(this.icon, new draw2d.layout.locator.XYAbsPortLocator({
+            x: 5,
+            y: 3,
+        }));
+        
+        return shape;
+    },
+
+    onContextMenu: function(x, y) {
+        // console.log(this);
+        clickRight(x, y, this);
+    },
+    
+    onDoubleClick: function() {
+        setLabel(this);
+    },
+
+    onClick: function (emitter, event) {
+        let xPos = this.x + this.width + 20;
+        let yPos = this.y;
+        showOptions(xPos, yPos, this.id, this.userData[1]);
+    },
+
+    setText: function(text) {
+        if (this.textFigure) {
+            this.remove(this.textFigure);
+        }
+        this.textFigure = new draw2d.shape.basic.Label({
+            text: text,
+            color: "#000000",
+            fontColor: "#000000",
+            stroke: 0
+        });
+        this.add(this.textFigure, new draw2d.layout.locator.CenterLocator(this));
     }
 
-    modalContent.appendChild(wrapperDiv);
-}
+});
 
-let selectedOption = "";
+var outputLocator = new draw2d.layout.locator.TopLocator;
+var inputLocator = new draw2d.layout.locator.BottomLocator;
+
+var MyConnection = draw2d.Connection.extend({
+    init: function(attr, setter, getter) {
+        this._super(attr, setter, getter);
+    
+        // Create a default arrow shape
+        var arrow = new draw2d.shape.basic.Polygon({
+            vertices: [
+                {x: 0, y: 0},
+                {x: -10, y: 10},
+                {x: -10, y: -10}
+            ],
+            stroke: 2,
+            color: "#000000",
+            bgColor: "#ffffff"
+        });
+    
+        // Add the arrow shape to the end of the connection
+        var locator = new draw2d.layout.locator.ManhattanMidpointLocator(this);
+        arrow.setPosition(locator.getX(), locator.getY());
+        this.add(arrow, locator);
+    }
+});
